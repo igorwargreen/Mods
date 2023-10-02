14dcb2028c4c07a59c4127570dafe1c9717d74b9
igorwargreen <igorwargreen@gmail.com>
2023-10-02 17:43:32 +0300

    Обновочка

---
+++ Strudeler's AraNap Civilizations 1.0.0/Core/Z.sql
@@ -0,0 +1,7 @@
+UPDATE Districts SET Cost=175, CostProgressionParam1=0 Where Cost=54;
+UPDATE Districts SET Cost=100, CostProgressionParam1=0 Where Cost=27;
+UPDATE Districts SET Cost=100, CostProgressionParam1=0 Where Cost=36;
+UPDATE Districts SET Cost=75, CostProgressionParam1=0 Where Cost=18;
+UPDATE Districts SET Cost=80, CostProgressionParam1=0 Where Cost=30;
+UPDATE Districts SET Cost=200, CostProgressionParam1=0 Where Cost=81;
+UPDATE Districts SET Cost=2000, CostProgressionParam1=0 Where Cost=1800;
