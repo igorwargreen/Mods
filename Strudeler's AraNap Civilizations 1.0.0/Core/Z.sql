7fcaa1280d85877b3714b2fecefec8bc1dc33468
igorwargreen <igorwargreen@gmail.com>
2023-10-02 19:29:55 +0300

    districts fix

--- Strudeler's AraNap Civilizations 1.0.0/Core/Z.sql
+++ Strudeler's AraNap Civilizations 1.0.0/Core/Z.sql
@@ -1,7 +1,8 @@
-UPDATE Districts SET Cost=175, CostProgressionParam1=0 Where Cost=54;
-UPDATE Districts SET Cost=100, CostProgressionParam1=0 Where Cost=27;
-UPDATE Districts SET Cost=100, CostProgressionParam1=0 Where Cost=36;
-UPDATE Districts SET Cost=75, CostProgressionParam1=0 Where Cost=18;
-UPDATE Districts SET Cost=80, CostProgressionParam1=0 Where Cost=30;
-UPDATE Districts SET Cost=200, CostProgressionParam1=0 Where Cost=81;
-UPDATE Districts SET Cost=2000, CostProgressionParam1=0 Where Cost=1800;
+UPDATE Districts SET Cost=150, CostProgressionParam1=0 Where Cost=54, CostProgressionParam1=40;
+UPDATE Districts SET Cost=85, CostProgressionParam1=0 Where Cost=27, CostProgressionParam1=40;
+UPDATE Districts SET Cost=85, CostProgressionParam1=0 Where Cost=36, CostProgressionParam1=1000;
+UPDATE Districts SET Cost=75, CostProgressionParam1=0 Where Cost=18, CostProgressionParam1=1000;
+UPDATE Districts SET Cost=80, CostProgressionParam1=0 Where Cost=30, CostProgressionParam1=40;
+UPDATE Districts SET Cost=175, CostProgressionParam1=0 Where Cost=81, CostProgressionParam1=1000;
+UPDATE Districts SET Cost=1800, CostProgressionParam1=0 Where Cost=1800, CostProgressionParam1=40;
+UPDATE Districts SET Cost=70, CostProgressionParam1=0 Where Cost=30, CostProgressionParam1=25;
