a518778f67013c3da12dae7c2711e9ed4f9b6184
igorwargreen <igorwargreen@gmail.com>
2023-10-03 15:46:40 +0300

    Cost

--- Strudeler's AraNap Civilizations 1.0.0/Core/Z.sql
+++ Strudeler's AraNap Civilizations 1.0.0/Core/Z.sql
@@ -1,8 +1,8 @@
-UPDATE Districts SET Cost=150, CostProgressionModel="NO_COST_PROGRESSION" Where Cost=54;
-UPDATE Districts SET Cost=85, CostProgressionModel="NO_COST_PROGRESSION" Where Cost=27;
-UPDATE Districts SET Cost=85, CostProgressionModel="NO_COST_PROGRESSION" Where Cost=36;
-UPDATE Districts SET Cost=75, CostProgressionModel="NO_COST_PROGRESSION" Where Cost=18;
-UPDATE Districts SET Cost=80, CostProgressionModel="NO_COST_PROGRESSION" Where Cost=30;
-UPDATE Districts SET Cost=175, CostProgressionModel="NO_COST_PROGRESSION" Where Cost=81;
+UPDATE Districts SET Cost=120, CostProgressionModel="COST_PROGRESSION_GAME_PROGRESS" Where Cost=54;
+UPDATE Districts SET Cost=75, CostProgressionModel="COST_PROGRESSION_GAME_PROGRESS" Where Cost=27;
+UPDATE Districts SET Cost=75, CostProgressionModel="COST_PROGRESSION_GAME_PROGRESS" Where Cost=36;
+UPDATE Districts SET Cost=60, CostProgressionModel="COST_PROGRESSION_GAME_PROGRESS" Where Cost=18;
+UPDATE Districts SET Cost=70, CostProgressionModel="COST_PROGRESSION_GAME_PROGRESS" Where Cost=30;
+UPDATE Districts SET Cost=200, CostProgressionModel="NO_COST_PROGRESSION" Where Cost=81;
 UPDATE Districts SET Cost=1800, CostProgressionModel="NO_COST_PROGRESSION" Where Cost=1800;
-UPDATE Districts SET Cost=70, CostProgressionModel="NO_COST_PROGRESSION" Where Cost=30;
+UPDATE Districts SET Cost=70, CostProgressionModel="COST_PROGRESSION_GAME_PROGRESS" Where Cost=30;
